08893f4d6f40efb3f08f851c7d0477c22e8f6e7f
Hà Hoàng Thạch Thảo <thao.ha@idtek.com.vn>
2022-09-21 15:19:08 +0700

    Update wPrice.js

--- cypress/pageObject/wPrice.js
+++ cypress/pageObject/wPrice.js
@@ -9,6 +9,7 @@ export const wPrice = {
         
     // Check mã các giá dầu
     checkFunction(){
+        cy.contains('[col-id="code"]', 'GPR-BRENT').should('exist'); // find the name
         cy.contains('[col-id="code"]', 'GPR-WTI-CLC1').should('exist'); // find the name
         cy.contains('[col-id="code"]', 'GPR-WTI').should('exist');      // find the name
         cy.contains('[col-id="code"]', 'GPR-LCOC1').should('exist');    // find the name
@@ -18,9 +19,10 @@ export const wPrice = {
         cy.contains('[col-id="code"]', 'GPR-GAS97').should('exist');    // find the name
         cy.contains('[col-id="code"]', 'GPR-GAS95').should('exist');    // find the name
         cy.contains('[col-id="code"]', 'GPR-GAS92').should('exist');    // find the name
-        cy.contains('[col-id="code"]', 'GPR-GAS92').should('exist');    // find the name
+        cy.contains('[col-id="code"]', 'GPR-GAS500').should('exist');    // find the name
         
         return this;
     }
-    
+
 }
+
